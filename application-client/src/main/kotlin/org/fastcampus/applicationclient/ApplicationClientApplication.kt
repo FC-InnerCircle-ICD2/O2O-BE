977f9a88e7d6da83dbda6e0c602d94a5132671a0
haeyonghahn <yong80211@gmail.com>
2025-01-10 01:00:47 +0900

    [CHORE] CI / CD 초기 구성
    admin-app 서비스 배포 CI / CD (O)
    client-app 서비스 배포 CI / CD (O)
    PR Test CI / CD (O)
    ktlint 자동 포맷팅 (X)

--- application-client/src/main/kotlin/org/fastcampus/applicationclient/ApplicationClientApplication.kt
+++ application-client/src/main/kotlin/org/fastcampus/applicationclient/ApplicationClientApplication.kt
@@ -1,6 +1,5 @@
 package org.fastcampus.applicationclient
 
-import org.fastcampus.member.Test
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -9,5 +8,4 @@ class ApplicationClientApplication
 
 fun main(args: Array<String>) {
     runApplication<ApplicationClientApplication>(*args)
-    val test = Test("sdfsdf")
 }
