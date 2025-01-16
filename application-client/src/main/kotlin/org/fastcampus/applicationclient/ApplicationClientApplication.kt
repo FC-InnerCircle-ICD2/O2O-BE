c5583a6882d9e1cca295c2da18b1cc923784e3c1
hyuk12 <hotman7025@naver.com>
2025-01-17 00:39:50 +0900

    [Chore] Not Found 에러 Test 통과(#14)
    <description>
    - Not Found 에러 Test 통과

--- application-client/src/main/kotlin/org/fastcampus/applicationclient/ApplicationClientApplication.kt
+++ application-client/src/main/kotlin/org/fastcampus/applicationclient/ApplicationClientApplication.kt
@@ -3,7 +3,7 @@ package org.fastcampus.applicationclient
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["org.fastcampus.applicationclient", "org.fastcampus.store"])
+@SpringBootApplication(scanBasePackages = ["org.fastcampus.applicationclient.store", "org.fastcampus.store"])
 class ApplicationClientApplication
 
 fun main(args: Array<String>) {
