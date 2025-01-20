b7c267411a88ee003349cf8ea40ccc3abbb9d4f3
seungbin.kim <seungbin.kim.dev@gmail.com>
2025-01-20 21:18:08 +0900

    [Feat] 결제 예외처리 추가 (#9)

--- application-client/src/main/kotlin/org/fastcampus/applicationclient/handler/ClientExceptionHandler.kt
+++ application-client/src/main/kotlin/org/fastcampus/applicationclient/handler/ClientExceptionHandler.kt
@@ -2,6 +2,7 @@ package org.fastcampus.applicationclient.handler
 
 import org.fastcampus.common.dto.APIResponseDTO
 import org.fastcampus.order.exception.OrderException
+import org.fastcampus.payment.exception.PaymentException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -24,6 +25,14 @@ class ClientExceptionHandler {
 
     @ExceptionHandler(OrderException::class)
     fun handleOrderException(exception: OrderException): ResponseEntity<APIResponseDTO<*>> {
+        logger.error("handleOrderException: ", exception)
+        return ResponseEntity
+            .status(exception.status)
+            .body(APIResponseDTO(exception.status, "FAIL", exception.message))
+    }
+
+    @ExceptionHandler(PaymentException::class)
+    fun handleOrderException(exception: PaymentException): ResponseEntity<APIResponseDTO<*>> {
         logger.error("handleOrderException: ", exception)
         return ResponseEntity
             .status(exception.status)
