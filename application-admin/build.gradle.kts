d9e04cc3d60b9526ba79be8c3aa4db2dd721eca5
Jin <tlsdydwls123@gmail.com>
2025-01-23 01:16:53 +0900

    [CHORE] application-admin 의존성 설정 (#8)
     application-admin 에서 주문 관련 의존성 및 spring component scan을
    설정했습니다.

--- application-admin/build.gradle.kts
+++ application-admin/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
 
 dependencies {
     implementation(project(":domains:store"))
+    implementation(project(":domains:order"))
+    implementation(project(":common"))
+    runtimeOnly(project(":infrastructure:order-postgres"))
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework:spring-tx")
