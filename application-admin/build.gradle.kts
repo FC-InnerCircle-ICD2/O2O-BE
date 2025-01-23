c1feba94c2f207663b421dd0c5a2865caa43d375
kk920106 <kk920106@naver.com>
2025-01-23 23:08:59 +0900

    fix: 관리자 로그인 API

--- application-admin/build.gradle.kts
+++ application-admin/build.gradle.kts
@@ -6,8 +6,14 @@ tasks.getByName("bootJar") {
 }
 
 dependencies {
+    implementation(project(":domains:member"))
     implementation(project(":domains:store"))
+    implementation(project(":common"))
+    runtimeOnly(project(":infrastructure:member-postgres"))
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework:spring-tx")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("com.auth0:java-jwt:4.2.1")
 }
--- application-admin/src/main/kotlin/org/fastcampus/applicationadmin/ApplicationAdminApplication.kt
+++ application-admin/src/main/kotlin/org/fastcampus/applicationadmin/ApplicationAdminApplication.kt
@@ -3,7 +3,12 @@ package org.fastcampus.applicationadmin
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = [
+        "org.fastcampus.applicationadmin",
+        "org.fastcampus.member",
+    ],
+)
 class ApplicationAdminApplication
 
 fun main(args: Array<String>) {
