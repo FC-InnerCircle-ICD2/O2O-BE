d1b2ca31576c5d9a7ad80a22493a5f7453f0c234
Jin <tlsdydwls123@gmail.com>
2025-01-28 16:29:38 +0900

    [Feat] 리뷰 등록 (#77)
    사용자의 리뷰 등록에 대한 API를 구현하였습니다. 내부적으로 이미지 등록을 위해 AWS S3 를 사용했습니다.

---
+++ infrastructure/review-postgres/src/main/kotlin/org/fastcampus/review/postgres/entity/ReviewJpaEntity.kt
@@ -0,0 +1,77 @@
+package org.fastcampus.review.postgres.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.fastcampus.common.entity.BaseEntity
+import org.fastcampus.review.entity.Review
+import org.fastcampus.review.entity.Review.DeliveryQuality
+
+@Entity
+@Table(name = "REVIEWS")
+class ReviewJpaEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    val id: Long? = null,
+    @Column(name = "ORDER_ID", nullable = false, length = 255)
+    val orderId: String,
+    @Column(name = "USER_ID", nullable = false)
+    val userId: Long,
+    @Column(nullable = false)
+    val storeId: String,
+    @Column(name = "CLIENT_REVIEW_CONTENT", nullable = false, length = 1000)
+    val clientReviewContent: String,
+    @Column(name = "TOTAL_SCORE", nullable = false)
+    val totalScore: Int,
+    @Column(name = "TASTE_SCORE", nullable = false)
+    val tasteScore: Int,
+    @Column(name = "QUANTITY_SCORE", nullable = false)
+    val quantityScore: Int,
+    @Column(name = "REPRESENTATIVE_IMAGE_URI", nullable = true, length = 500)
+    val representativeImageUri: String? = null,
+    @Enumerated(EnumType.STRING)
+    @Column(name = "DELIVERY_QUALITY", nullable = true, length = 50)
+    val deliveryQuality: DeliveryQuality? = null,
+    @Column(name = "ADMIN_USER_ID", nullable = true)
+    val adminUserId: Long? = null,
+    @Column(name = "ADMIN_REVIEW_CONTENT", nullable = true, length = 1000)
+    val adminReviewContent: String? = null,
+) : BaseEntity()
+
+fun Review.toJpaEntity() =
+    ReviewJpaEntity(
+        id = this.id,
+        orderId = this.orderId,
+        userId = this.userId,
+        storeId = this.storeId,
+        clientReviewContent = this.clientReviewContent,
+        totalScore = this.totalScore,
+        tasteScore = this.tasteScore,
+        quantityScore = this.quantityScore,
+        representativeImageUri = this.representativeImageUri,
+        deliveryQuality = this.deliveryQuality,
+        adminUserId = this.adminUserId,
+        adminReviewContent = this.adminReviewContent,
+    )
+
+fun ReviewJpaEntity.toModel() =
+    Review(
+        id = this.id,
+        orderId = this.orderId,
+        userId = this.userId,
+        storeId = this.storeId,
+        clientReviewContent = this.clientReviewContent,
+        totalScore = this.totalScore,
+        tasteScore = this.tasteScore,
+        quantityScore = this.quantityScore,
+        representativeImageUri = this.representativeImageUri,
+        deliveryQuality = this.deliveryQuality,
+        adminUserId = this.adminUserId,
+        adminReviewContent = this.adminReviewContent,
+    )
