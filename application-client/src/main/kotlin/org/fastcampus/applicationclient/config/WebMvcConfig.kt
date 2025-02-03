e005b1cf5a99999b8bcae05c6854723458de098d
haeyonghahn <yong80211@gmail.com>
2025-02-03 21:37:22 +0900

    [Test] WebMvcConfig 수정

--- application-client/src/main/kotlin/org/fastcampus/applicationclient/config/WebMvcConfig.kt
+++ application-client/src/main/kotlin/org/fastcampus/applicationclient/config/WebMvcConfig.kt
@@ -1,16 +1,22 @@
 package org.fastcampus.applicationclient.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebMvcConfig : WebMvcConfigurer {
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry
-            .addMapping("/**")
-            .allowedHeaders("*")
-            .allowedMethods("*")
-            .allowedOrigins("*")
+class WebMvcConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://13.124.27.138")
+                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD")
+                    .allowedHeaders("*")
+            }
+        }
     }
 }
