7bc88ff90fbd7f8279270702f037c7112b927890
kk920106 <kk920106@naver.com>
2025-02-12 23:53:28 +0900

    주소 등록 API

--- application-client/src/main/kotlin/org/fastcampus/applicationclient/member/dto/request/MemberAddressCreateRequest.kt
+++ application-client/src/main/kotlin/org/fastcampus/applicationclient/member/dto/request/MemberAddressCreateRequest.kt
@@ -1,10 +1,11 @@
 package org.fastcampus.applicationclient.member.dto.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import org.fastcampus.member.code.MemberAddressType
 
 data class MemberAddressCreateRequest(
-    @field:NotBlank(message = "주소 타입을 입력해 주세요.")
+    @field:NotNull(message = "주소 타입을 입력해 주세요.")
     val addressType: MemberAddressType?,
     @field:NotBlank(message = "도로명 주소를 입력해 주세요.")
     val roadAddress: String?,
@@ -12,8 +13,8 @@ data class MemberAddressCreateRequest(
     val jibunAddress: String?,
     val detailAddress: String?,
     val alias: String?,
-    @field:NotBlank(message = "위도를 입력해 주세요.")
+    @field:NotNull(message = "위도를 입력해 주세요.")
     val latitude: Double?,
-    @field:NotBlank(message = "경도를 입력해 주세요.")
+    @field:NotNull(message = "경도를 입력해 주세요.")
     val longitude: Double?,
 )
