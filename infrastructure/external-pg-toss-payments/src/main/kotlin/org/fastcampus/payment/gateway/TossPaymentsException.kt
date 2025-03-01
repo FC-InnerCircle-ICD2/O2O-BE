6f57f8cfeec24a0bb8b6f43c02c4a94507ae4dd0
seungbin.kim <seungbin.kim.dev@gmail.com>
2025-03-01 11:33:29 +0900

    [Feat] PG 결제승인 수정 (#277)

--- infrastructure/external-pg-toss-payments/src/main/kotlin/org/fastcampus/payment/gateway/TossPaymentsException.kt
+++
@@ -1,6 +0,0 @@
-package org.fastcampus.payment.gateway
-
-internal data class TossPaymentsException(
-    override val message: String,
-    val error: TossPaymentsErrorResponse? = null,
-) : RuntimeException(message)
--- infrastructure/external-pg-toss-payments/src/main/kotlin/org/fastcampus/payment/gateway/client/TossPaymentsClient.kt
+++ infrastructure/external-pg-toss-payments/src/main/kotlin/org/fastcampus/payment/gateway/client/TossPaymentsClient.kt
@@ -1,9 +1,9 @@
-package org.fastcampus.payment.gateway
+package org.fastcampus.payment.gateway.client
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 
-@FeignClient(name = "tossPaymentsClient", url = "\${external.tosspayments.url}", configuration = [TossPaymentsClientConfig::class])
+@FeignClient(name = "tossPaymentsClient", url = "\${external.tosspayments.url}")
 interface TossPaymentsClient {
     @PostMapping("/v1/payments/confirm")
     fun approve(request: TossPaymentsApproveRequest): TossPaymentsResponse
